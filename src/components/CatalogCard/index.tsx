4a77ea569538804befc04ba0730e31b11027a458
Joao Lucas <joaolribeiro0811@gmail.com>
2023-11-27 10:34:30 -0300

    Dynamic catalog rendering and adding links

--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -1,5 +1,6 @@
 import './styles.css';
 import { ProductDTO } from '../../models/product';
+import { Link } from 'react-router-dom';
 
 type Props = {
     product: ProductDTO
@@ -7,16 +8,19 @@ type Props = {
 
 export default function CatalogCard({ product }: Props) {
     return (
-        <div className="dsc-catalog-card">
-            <div className="dsc-catalog-card-image">
-                <img src={product.imgUrl} alt={product.name} />
+        <Link to={`/product-details/${product.id}`}>
+            <div className="dsc-catalog-card">
+                <div className="dsc-catalog-card-image">
+                    <img src={product.imgUrl} alt={product.name} />
+                </div>
+                <div className="dsc-catalog-card-price">
+                    <h3>R$ {product.price.toFixed(2)}</h3>
+                </div>
+                <div className="dsc-catalog-card-name-product">
+                    <h3>{product.name}</h3>
+                </div>
             </div>
-            <div className="dsc-catalog-card-price">
-                <h3>R$ {product.price.toFixed(2)}</h3>
-            </div>
-            <div className="dsc-catalog-card-name-product">
-                <h3>{product.name}</h3>
-            </div>
-        </div>
+        </Link>
+
     );
 }
--- src/components/HeaderClient/index.tsx
+++ src/components/HeaderClient/index.tsx
@@ -1,17 +1,24 @@
 import './styles.css';
 import cartIcon from '../../assets/cart.svg';
+import { Link } from 'react-router-dom';
 
 export default function HeaderClient() {
 
     return (
         <header className="dsc-header-client">
             <nav className="dsc-container">
-                <h1>DSCommerce</h1>
+                <Link to='/'>
+                    <h1>DSCommerce</h1>
+                </Link>
                 <div className="dsc-user">
                     <div className="dsc-cart-icon">
-                        <img src={cartIcon} alt="Carrinho" />
+                        <Link to='/cart'>
+                            <img src={cartIcon} alt="Carrinho" />
+                        </Link>
                     </div>
-                    <a href="#">Entrar</a>
+                    <Link to='/login'>
+                        Entrar
+                    </Link>
                 </div>
             </nav>
         </header>
