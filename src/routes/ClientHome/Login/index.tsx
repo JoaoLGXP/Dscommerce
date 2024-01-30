ff9d7ea605eee427736e48003261c94b40d92e86
Joao Lucas <joaolribeiro0811@gmail.com>
2024-01-30 08:58:23 -0300

    Forms update

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import './styles.css';
-import { useContext, useState } from 'react';
-import ButtonPrimary from '../../../components/ButtonPrimary';
-import * as authService from '../../../services/auth-service';
-import { useNavigate } from 'react-router-dom';
-import { ContextToken } from '../../../utils/context-token';
-import FormInput from '../../../components/FormInput';
+import "./styles.css";
+import { useContext, useState } from "react";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import * as authService from "../../../services/auth-service";
+import * as forms from "../../../utils/forms";
+import { useNavigate } from "react-router-dom";
+import { ContextToken } from "../../../utils/context-token";
+import FormInput from "../../../components/FormInput";
 
 export default function Login() {
 
@@ -48,9 +49,7 @@ export default function Login() {
     }
 
     function handleInputChange(event: any) {
-        const value = event.target.value;
-        const name = event.target.name;
-        setFormData({ ...formData, [name]: { ...formData[name], value: value } });
+        setFormData(forms.update(formData, event.target.name, event.target.value));
     }
 
     return (
