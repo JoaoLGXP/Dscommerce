8057fbd30f885e5cd286c61beeecb6ee2df45d22
Joao Lucas <joaolribeiro0811@gmail.com>
2024-01-30 08:40:49 -0300

    Custom FormInput

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -2,10 +2,10 @@
 import './styles.css';
 import { useContext, useState } from 'react';
 import ButtonPrimary from '../../../components/ButtonPrimary';
-import { CredentialsDTO } from '../../../models/auth';
 import * as authService from '../../../services/auth-service';
 import { useNavigate } from 'react-router-dom';
 import { ContextToken } from '../../../utils/context-token';
+import FormInput from '../../../components/FormInput';
 
 export default function Login() {
 
@@ -13,14 +13,30 @@ export default function Login() {
 
     const navigate = useNavigate();
 
-    const [formData, setFormData] = useState<CredentialsDTO>({
-        username: '',
-        password: ''
+    const [formData, setFormData] = useState<any>({
+        username: {
+            value: "",
+            id: "username",
+            name: "username",
+            type: "text",
+            placeholder: "Email",
+            validation: function (value: string) {
+                return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value.toLowerCase());
+            },
+            message: "Favor informar um email válido",
+        },
+        password: {
+            value: "",
+            id: "password",
+            name: "password",
+            type: "password",
+            placeholder: "Senha",
+        }
     })
 
     function handleSubmit(event: any) {
         event.preventDefault();
-        authService.loginRequest(formData)
+        authService.loginRequest({ username: formData.username.value, password: formData.password.value })
             .then(response => {
                 authService.saveAccessToken(response.data.access_token);
                 setContextTokenPayload(authService.getAccessTokenPayload());
@@ -34,7 +50,7 @@ export default function Login() {
     function handleInputChange(event: any) {
         const value = event.target.value;
         const name = event.target.name;
-        setFormData({...formData, [name]: value});
+        setFormData({ ...formData, [name]: { ...formData[name], value: value } });
     }
 
     return (
@@ -44,29 +60,23 @@ export default function Login() {
                     <h2>LOGIN</h2>
                     <div className="dsc-login-access-container">
                         <div>
-                            <input
-                                name="username"
-                                value={formData.username}
+                            <FormInput
+                                {...formData.username}
                                 className="dsc-login-input"
-                                type="text"
-                                placeholder="Email"
                                 onChange={handleInputChange}
                             />
                             <div className="dsc-error-message"></div>
                         </div>
                         <div>
-                            <input
-                                name="password"
-                                value={formData.password}
+                            <FormInput
+                                {...formData.password}
                                 className="dsc-login-input dsc-margin-bottom"
-                                type="password"
-                                placeholder="Senha"
                                 onChange={handleInputChange}
                             />
                             <div className="dsc-error-message"></div>
                         </div>
                     </div>
-                    <ButtonPrimary text='Entrar'/>
+                    <ButtonPrimary text='Entrar' />
                 </form>
             </section>
         </main>
